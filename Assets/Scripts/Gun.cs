258116c8ef3b0e8ceeee7078661c55e9bea5633b
Andrew Lamy <lamyman11@gmail.com>
2021-10-05 12:32:34 -0400

    Chapter 7, Added Power-Ups

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -37,6 +37,9 @@ public class Gun : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform launchPosition;
     private AudioSource audioSource;
+    public bool isUpgraded;
+    public float upgradeTime = 10.0f;
+    private float currentTime;
 
     // Use this for initialization
     void Start ()
@@ -59,12 +62,41 @@ public class Gun : MonoBehaviour
             CancelInvoke("fireBullet");
         }
 	}
-
-    void fireBullet()
+    
+    private Rigidbody createBullet()
     {
         GameObject bullet = Instantiate(bulletPrefab) as GameObject;
         bullet.transform.position = launchPosition.position;
-        bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100;
+        return bullet.GetComponent<Rigidbody>();
+    }
+
+    void fireBullet()
+    {
+        Rigidbody bullet = createBullet();
+        bullet.velocity = transform.parent.forward * 100;
+    }
+    if (isUpgraded)
+    {
+        Rigidbody bullet2 = createBullet();
+        bullet2.velocity =
+        (transform.right + transform.forward / 0.5f) * 100;
+        Rigidbody bullet3 = createBullet();
+        bullet3.velocity =
+        ((transform.right* -1) + transform.forward / 0.5f) * 100;
+    }
+
+    if (isUpgraded)
+    {
+        audioSource.PlayOneShot(SoundManager.Instance.upgradedGunFire);
+    }
+    else
+    {
         audioSource.PlayOneShot(SoundManager.Instance.gunFire);
     }
+
+public void UpgradeGun()
+{
+    isUpgraded = true;
+    currentTime = 0;
+}
 }
