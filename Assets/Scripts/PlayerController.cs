cb085b02ebe1701069fc0bdf0ad230cbf25ccc67
Andrew Lamy <lamyman11@gmail.com>
2021-10-11 23:05:13 -0400

    Chapter 8, Killing the Hero & Managing Script Order

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -34,66 +34,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public Rigidbody head;
     public float moveSpeed = 50.0f;
-    private CharacterController characterController;
+    public Rigidbody head;
     public LayerMask layerMask;
-    private Vector3 currentLookTarget = Vector3.zero;
+    public Animator bodyAnimator;
     public float[] hitForce;
     public float timeBetweenHits = 2.5f;
+    public Rigidbody marineBody;
+    private bool isDead = false;
     private bool isHit = false;
     private float timeSinceHit = 0;
     private int hitNumber = -1;
+    private Vector3 currentLookTarget = Vector3.zero;
+    private CharacterController characterController;
+    private DeathParticles deathParticles;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         characterController = GetComponent<CharacterController>();
+        deathParticles = gameObject.GetComponentInChildren<DeathParticles>();
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
-       0, Input.GetAxis("Vertical"));
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
-
-        void FixedUpdate()
-        {
-            Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
- 0, Input.GetAxis("Vertical"));
-            if (moveDirection == Vector3.zero)
-            {
-                // TODO
-            }
-            else
-            {
-                head.AddForce(transform.right * 150, ForceMode.Acceleration);
-            }
-
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
-
-            if (Physics.Raycast(ray, out hit, 1000, layerMask, QueryTriggerInteraction.Ignore))
-            {
-                if (hit.point != currentLookTarget)
-                {
-                    currentLookTarget = hit.point;
-                }
-            }
-            // 1
-            Vector3 targetPosition = new Vector3(hit.point.x,
-             transform.position.y, hit.point.z);
-            // 2
-            Quaternion rotation = Quaternion.LookRotation(targetPosition -
-             transform.position);
-            // 3
-            transform.rotation = Quaternion.Lerp(transform.rotation,
-             rotation, Time.deltaTime * 10.0f);
-        }
-
         if (isHit)
         {
             timeSinceHit += Time.deltaTime;
@@ -105,6 +72,34 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (moveDirection == Vector3.zero)
+        {
+            bodyAnimator.SetBool("IsMoving", false);
+        }
+        else
+        {
+            head.AddForce(transform.right * 150, ForceMode.Acceleration);
+            bodyAnimator.SetBool("IsMoving", true);
+        }
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
+
+        if (Physics.Raycast(ray, out hit, 1000, layerMask, QueryTriggerInteraction.Ignore))
+        {
+            if (hit.point != currentLookTarget)
+            {
+                currentLookTarget = hit.point;
+            }
+        }
+        Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+        Quaternion rotation = Quaternion.LookRotation(targetPosition - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10.0f);
+    }
+
     void OnTriggerEnter(Collider other)
     {
         Alien alien = other.gameObject.GetComponent<Alien>();
@@ -113,20 +108,39 @@ public class PlayerController : MonoBehaviour
             if (!isHit)
             {
                 hitNumber += 1; // 2
-                CameraShake cameraShake = Camera.main.GetComponent<CameraShake>();
-                if (hitNumber < hitForce.Length) // 3
+                CameraShake cameraShake =
+                    Camera.main.GetComponent<CameraShake>();
+                if (hitNumber < hitForce.Length) // 3 
                 {
                     cameraShake.intensity = hitForce[hitNumber];
                     cameraShake.Shake();
                 }
                 else
                 {
-                    // death todo
+                    Die();
                 }
                 isHit = true; // 4
                 SoundManager.Instance.PlayOneShot(SoundManager.Instance.hurt);
             }
             alien.Die();
         }
-
     }
+
+    public void Die()
+    {
+        bodyAnimator.SetBool("IsMoving", false);
+        marineBody.transform.parent = null;
+        marineBody.isKinematic = false;
+        marineBody.useGravity = true;
+        marineBody.gameObject.GetComponent<CapsuleCollider>().enabled = true;
+        marineBody.gameObject.GetComponent<Gun>().enabled = false;
+        Destroy(head.gameObject.GetComponent<HingeJoint>());
+        head.transform.parent = null;
+        head.useGravity = true;
+        SoundManager.Instance.PlayOneShot(SoundManager.Instance.marineDeath);
+        deathParticles.Activate();
+        Destroy(gameObject);
+    }
+
+
+}
